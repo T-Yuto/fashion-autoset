25827bfb45b65dbed9a1393ecf3f6a0e46089a26
T-Yuto <u10.tk_tf.onnt@icloud.com>
2020-05-12 23:23:36 +0900

    編集画面ー値がからの時カラムの削除をコントローラーで設定ーーの削除ボタンの追加、

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "images#index"
-  resources :images, only: [:new, :create, :index, :edit, :update]
+  resources :images, only: [:new, :create, :index, :edit, :update, :destroy]
   resources :users, only: [:show]
 end
