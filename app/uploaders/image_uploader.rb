168bef063306e7c9f7f79582241c7fa705bf47fe
T-Yuto <u10.tk_tf.onnt@icloud.com>
2020-06-12 12:49:15 +0900

    s3設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,7 +30,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-    process resize_to_fit: [400, 500]
+  process resize_to_fit: [400, 500]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
@@ -44,4 +44,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 end
